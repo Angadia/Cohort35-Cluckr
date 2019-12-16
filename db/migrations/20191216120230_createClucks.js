052b6f308875a164367ae22e8ff77cdf814d2d76
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2019-12-16 12:27:38 -0800

    Added Cluckr postgres database using knex.

---
+++ db/migrations/20191216120230_createClucks.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("clucks", (t) => {
+    t.bigIncrements("id");
+    t.string("username");
+    t.string("image_url");
+    t.text("content");
+    t.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("clucks");
+};
