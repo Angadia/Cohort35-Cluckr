0289b41d88a82fba53763a860c307890c86177fd
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2019-12-16 11:38:35 -0800

    Added cookie based authentication.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+
+const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
+
+router.get('/sign_in', (req, res) => {
+  if (!req.cookies.username) {
+    res.locals.signedInUser = '';
+    res.render('sign_in');
+  } else {
+    res.redirect('/');
+  };
+});
+
+router.post('/sign_in', (req, res) => {
+  const { username } = req.body;
+  if (!username && !req.cookies.username) {
+    res.redirect('/sign_in');
+  }
+  else {
+    if (!username) {
+      username = req.cookies.username;
+    };
+    res.cookie('username', username, { maxAge: ONE_WEEK });
+    res.redirect('/');
+  };
+});
+
+router.post('/sign_out', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/');
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
