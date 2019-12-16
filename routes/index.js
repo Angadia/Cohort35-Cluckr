eb8711aba0ff64305a8cfca641866ce232df8b36
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2019-12-16 14:07:43 -0800

    Added human friendly creation date.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-var express = require('express');
+const express = require('express');
 const knex = require("../db/client");
+const TimeAgo = require("javascript-time-ago");
+const en = require("javascript-time-ago/locale/en");
 
-var router = express.Router();
+TimeAgo.addLocale(en);
 
+const timeAgo = new TimeAgo('en-US');
+
+const router = express.Router();
 
 const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 
@@ -43,6 +48,9 @@ router.get('/', (req, res) => {
     .select('*')
     .orderBy('created_at', 'DESC')
   .then( clucks => {
+    clucks.forEach(cluck => {
+      cluck['time_ago'] = timeAgo.format(cluck.created_at);
+    });
     res.locals.clucks = clucks;
     res.render("index");
   });
